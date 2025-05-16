f9fe795654f0c7703fdfe909ba481e5fd1b4e3c1
Jayesh Sutar <jayeshsutar1108@gmail.com>
2025-05-16 12:12:39 +0530

    Added Login page with Tailwind CSS configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AuditDesk';
+  valid = false;
+
 }
